fcc08715f05b5186928bc615d417f94883378401
dmdelfim@gmail.com <47758349+DanielDelfim@users.noreply.github.com>
2019-11-13 21:33:00 -0300

    Projeto criado

---
+++ src/main/java/com/danieldelfim/cursomct/CursomctApplication.java
@@ -0,0 +1,13 @@
+package com.danieldelfim.cursomct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danieldelfim/cursomct/CursomctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieldelfim.cursomct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
