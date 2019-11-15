bbcd3b7f9933a7fd75b6f79699bacb8afc98973d
dmdelfim@gmail.com <47758349+DanielDelfim@users.noreply.github.com>
2019-11-15 10:37:52 -0300

    Criando repositório e serviços para Categoria

---
+++ src/main/java/com/danieldelfim/cursomct/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.danieldelfim.cursomct.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danieldelfim.cursomct.domain.Categoria;
+import com.danieldelfim.cursomct.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired //instancia automaticamente a dependência dentro de uma classe. Dá acesso
+	private CategoriaRepository repo;
+	
+	//agora precisamos usar o objeto repo para ir no banco de dados e buscar uma categoria conforme ID
+	
+	public Categoria buscar(Integer id) { //método buscar tem que receber o id como parâmetro
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
