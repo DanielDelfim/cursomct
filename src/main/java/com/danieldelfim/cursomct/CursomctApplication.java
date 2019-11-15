eae53899c83faa0c8eef35a66afd64fe025b9725
dmdelfim@gmail.com <47758349+DanielDelfim@users.noreply.github.com>
2019-11-15 10:55:51 -0300

    Criando operação de instanciação

--- src/main/java/com/danieldelfim/cursomct/CursomctApplication.java
+++ src/main/java/com/danieldelfim/cursomct/CursomctApplication.java
@@ -1,13 +1,37 @@
 package com.danieldelfim.cursomct;
 
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomctApplication {
+import com.danieldelfim.cursomct.domain.Categoria;
+import com.danieldelfim.cursomct.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomctApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomctApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Informática"); //null pois id gerado automatico
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		//preciso salvar estes dados no BD. Somente Repository pode e por isso coloca uma dependencia dele
+	
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	
 	}
 
 }
